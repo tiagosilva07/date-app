d46bf5b8d0aa072b2b9ca2a091022886cb9253da
Tiago <tiagosilva@weget.pt>
2022-04-09 14:22:24 +0100

    Add project files.

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        
+    }
+}
